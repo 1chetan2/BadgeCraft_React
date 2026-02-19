409ee652abfe93bcdfb9e1c9f8484b65809379dd
Chetan Patil <crp13749@gmail.com>
2026-02-19 17:43:05 +0530

    user and badges ui added

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+import BadgeTemplates from "./pages/BadgeTemplates";
+
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/badges" element={<BadgeTemplates />} />
+
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { login } from "../services/authService";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    remember: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    //  clear old token
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
+
+    const res = await login({
+      email: formData.email,
+      password: formData.password,
+    });
+
+    const token = res.data.token;
+
+    //  store token properly
+    if (formData.remember) {
+      localStorage.setItem("token", token);
+    } else {
+      sessionStorage.setItem("token", token);
+    }
+
+    navigate("/dashboard");
+  } catch (error) {
+    alert("Invalid credentials");
+  }
+};
+
+  return (
+    <div className="auth-container">
+      <div className="card">
+        <h2>BadgeCraft</h2>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            onChange={handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handleChange}
+            required
+          />
+
+          <div className="remember">
+            <label className="remember-label">
+              <input type="checkbox" name="remember" onChange={handleChange} />
+              Remember Me
+            </label>
+          </div>
+
+          <button type="submit">Sign In</button>
+        </form>
+
+        <p>
+          Don’t have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
